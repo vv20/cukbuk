edeb2933a9ca19085617e2337cac1e5b6ab69a6b
vv20 <vv20@st-andrews.ac.uk>
2022-06-16 20:50:39 +0100

    Changed production secret key to be generated at runtime

--- cukbuk_container/settings/production.py
+++ cukbuk_container/settings/production.py
@@ -1,7 +1,9 @@
+from django.core.management import utils
 import os
 from .common import *
 
 DEBUG = False
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = utils.get_random_secret_key()
+#with open('/etc/secret_key.txt') as f:
+    #SECRET_KEY = f.read().strip()
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', os.environ['HOSTNAME']]
