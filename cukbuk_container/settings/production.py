c7b59427afba49368c409e2714f5bb806e971e73
vv20 <vv20@st-andrews.ac.uk>
2022-06-19 22:19:09 +0100

    Clean up from production database setup

--- cukbuk_container/settings/production.py
+++ cukbuk_container/settings/production.py
@@ -14,10 +14,10 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost', os.environ['HOSTNAME']]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cukbuk_db',
+        'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
-        'PORT': '3306',
+        'PORT': os.environ['DB_PORT'],
     }
 }
